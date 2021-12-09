cfef2eb97908faa8f73bc00132f631c7f558cc87
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2021-12-10 01:02:08 +0500

    moved server.js file

---
+++ server.js
@@ -0,0 +1,32 @@
+// findNemo = (text) => {
+//   nameCartoon = "nemo";
+//   const nemoIndex = String(text.indexOf([nameCartoon]) + 1);
+//   return console.log(`Nemo ${nemoIndex} o'rinda topdim`);
+// };
+
+// findNemo("ajoyib nemo multik");
+
+// const toArray = (obj) => {
+//   const result = [];
+//   for (let i in obj) {
+//     result.push(Array(i, obj[i]));
+//   }
+//   return result;
+// };
+
+// console.log(toArray({ a: 1, b: 2, c: 3 }));
+
+// const isAnagram = (str) => str == str.split("").reverse().join("");
+
+// console.log(isAnagram("non"));
+
+// const salom = (str) => {
+//   const str === str.toUpperCase();
+//   return console.log(upperCaseLatters);
+// };
+
+// console.log(salom("saloM"));
+
+
+const weightMark = 78;
+const  heightMark = 1.69
